32b5caec90d2b107eb3da35112edbb94708630a3
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-09-22 16:45:34 +0300

    build dossier script update

--- bin/build_dossier.js
+++ bin/build_dossier.js
@@ -1,22 +1,19 @@
 const DOSSIER_SEED_FILE_PATH = "./seed";
 const BRICK_STORAGE_ENDPOINT = process.env.SSAPPS_FAVORITE_EDFS_ENDPOINT || "http://127.0.0.1:8080";
+const DEFAULT_DOMAIN = "default";
 
 require("./../../../privatesky/psknode/bundles/csbBoot.js");
-require("./../../../privatesky/psknode/bundles/edfsBar.js");
+require("./../../../privatesky/psknode/bundles/openDSU.js");
 const fs = require("fs");
-const EDFS = require("edfs");
-$$.BDNS.addConfig("default", {
-    endpoints: [
-        {
-            endpoint: BRICK_STORAGE_ENDPOINT,
-            type: 'brickStorage'
-        },
-        {
-            endpoint: BRICK_STORAGE_ENDPOINT,
-            type: 'anchorService'
-        }
-    ]
+const openDSU = require("opendsu");
+const bdns = openDSU.loadApi("bdns");
+const keyssi = openDSU.loadApi("keyssi");
+const resolver = openDSU.loadApi("resolver");
+bdns.addRawInfo(DEFAULT_DOMAIN, {
+    brickStorages: [BRICK_STORAGE_ENDPOINT],
+    anchoringServices: [BRICK_STORAGE_ENDPOINT]
 })
+
 function storeKeySSI(seed_path, keySSI, callback) {
     fs.writeFile(seed_path, keySSI, (err) => {
         return callback(err, keySSI);
@@ -24,7 +21,7 @@ function storeKeySSI(seed_path, keySSI, callback) {
 }
 
 function createDossier(callback) {
-    EDFS.createDSU("Bar", (err, bar) => {
+    resolver.createDSU(keyssi.buildSeedSSI(DEFAULT_DOMAIN), (err, bar) => {
         if (err) {
             return callback(err);
         }
@@ -34,8 +31,8 @@ function createDossier(callback) {
 }
 
 function updateDossier(bar, callback) {
-    bar.delete("/", function (err) {
-        if (err) {
+    bar.delete("/", function(err){
+        if(err){
             throw err;
         }
 
@@ -43,6 +40,7 @@ function updateDossier(bar, callback) {
             if (err) {
                 return callback(err);
             }
+
             bar.getKeySSI((err, keySSI) => {
                 if (err) {
                     return callback(err);
@@ -62,19 +60,19 @@ function build_dossier(callback) {
 
         let keySSI;
         try {
-            keySSI = require("key-ssi-resolver").KeySSIFactory.create(content.toString());
+            keySSI = keyssi.parse(content.toString());
         } catch (err) {
-            console.log("Invalid KeySSI. Creating a new Dossier...");
+            console.log("Invalid seed. Creating a new Dossier...");
             return createDossier(callback);
         }
 
-        if (keySSI.getHint() !== BRICK_STORAGE_ENDPOINT) {
+        if(keySSI.getHint() !== BRICK_STORAGE_ENDPOINT){
             console.log("Endpoint change detected. Creating a new Dossier...");
             return createDossier(callback);
         }
 
         console.log("Dossier updating...");
-        EDFS.resolveSSI(content.toString(), "Bar", (err, bar) => {
+        resolver.loadDSU(content.toString(), (err, bar) => {
             if (err) {
                 return callback(err);
             }
